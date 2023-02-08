0a8bfaf0ed9edd7b7dd35e56f9047d4d9a7d103d
Jacob Kemple <JacobKemple@outlook.com>
2023-02-07 22:33:17 -0700

    Cleaned up code briefly
    - Use proper MVVM
    - Use cleaner UI
    - Error checks

--- src/UserSearch/UserSearch/MainWindow.xaml.cs
+++ src/UserSearch/UserSearch/MainWindow.xaml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 using UserSearch.Models;
 using UserSearch.Services;
+using UserSearch.ViewModels;
 
 namespace UserSearch
 {
@@ -21,86 +18,87 @@ namespace UserSearch
             InitializeComponent();
             _serializationService = new XmlSerializationService();
             _repository = new UserRepository(_serializationService);
+            DataContext = new MainWindowViewModel();
         }
-        private static (Label label, TextBlock textBlock) CreateLableWithTextBlock(string labelText, string textBlockText)
-        {
-            var label = string.Equals(labelText, "client id", StringComparison.InvariantCultureIgnoreCase) ?
-                new Label { Content = labelText, HorizontalAlignment = HorizontalAlignment.Left, FontWeight = FontWeights.UltraBold, FontSize = 15.0 } :
-                new Label { Content = labelText, HorizontalAlignment = HorizontalAlignment.Left, FontWeight = FontWeights.Bold, FontSize = 10.0 };
+        //private static (Label label, TextBlock textBlock) CreateLableWithTextBlock(string labelText, string textBlockText)
+        //{
+        //    var label = string.Equals(labelText, "client id", StringComparison.InvariantCultureIgnoreCase) ?
+        //        new Label { Content = labelText, HorizontalAlignment = HorizontalAlignment.Left, FontWeight = FontWeights.UltraBold, FontSize = 15.0 } :
+        //        new Label { Content = labelText, HorizontalAlignment = HorizontalAlignment.Left, FontWeight = FontWeights.Bold, FontSize = 10.0 };
 
-            var textBox = new TextBlock { Text = textBlockText, Margin = new Thickness(5) };
-            return (label, textBox);
-        }
-        private void FindByFirstName_Clicked(object sender, RoutedEventArgs e)
-        {
-            var firstName = firstNameOnlyTextBox.Text.ToLowerInvariant();
-            _panel.Children.Clear();
-            foreach (var user in _repository.GetAll().Where(user => user.FirstName.ToLowerInvariant() == firstName).ToList())
-            {
-                AddUserToStackPanel(user);
-            }
-            _panel.Focus();
-            _panel.BringIntoView();
-        }
+        //    var textBox = new TextBlock { Text = textBlockText, Margin = new Thickness(5) };
+        //    return (label, textBox);
+        //}
+        //private void FindByFirstName_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    var firstName = firstNameOnlyTextBox.Text.ToLowerInvariant();
+        //    _panel.Children.Clear();
+        //    foreach (var user in _repository.GetAll().Where(user => user.FirstName.ToLowerInvariant() == firstName).ToList())
+        //    {
+        //        AddUserToStackPanel(user);
+        //    }
+        //    _panel.Focus();
+        //    _panel.BringIntoView();
+        //}
 
-        private void FindByLastName_Clicked(object sender, RoutedEventArgs e)
-        {
-            var firstName = lastNameOnlyTextBox.Text.ToLowerInvariant();
-            _panel.Children.Clear();
-            foreach (var user in _repository.GetAll().Where(user => user.LastName.ToLowerInvariant() == firstName).ToList())
-            {
-                AddUserToStackPanel(user);
-            }
-            _panel.Focus();
-            _panel.BringIntoView();
-        }
-        private void FindByFirstAndLastName_Clicked(object sender, RoutedEventArgs e)
-        {
-            var firstName = firstNameTextBox.Text.ToLowerInvariant();
-            var lastName = lastNameTextBox.Text.ToLowerInvariant();
-            _panel.Children.Clear();
-            foreach (var user in _repository.GetAll().Where(user => user.FirstName.ToLowerInvariant() == firstName && user.LastName.ToLowerInvariant() == lastName).ToList())
-            {
-                AddUserToStackPanel(user);
-            }
-            _panel.Focus();
-            _panel.BringIntoView();
-        }
-        private void FindByEmail_Clicked(object sender, RoutedEventArgs e)
-        {
-            var email = emailTextBox.Text.ToLowerInvariant();
-            _panel.Children.Clear();
-            foreach (var user in _repository.GetAll().Where(user => user.Email.ToLowerInvariant() == email).ToList())
-            {
-                AddUserToStackPanel(user);
-            }
-            _panel.Focus();
-            _panel.BringIntoView();
-        }
-        private void OnLoad(object sender, RoutedEventArgs e)
-        {
-        }
+        //private void FindByLastName_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    var firstName = lastNameOnlyTextBox.Text.ToLowerInvariant();
+        //    _panel.Children.Clear();
+        //    foreach (var user in _repository.GetAll().Where(user => user.LastName.ToLowerInvariant() == firstName).ToList())
+        //    {
+        //        AddUserToStackPanel(user);
+        //    }
+        //    _panel.Focus();
+        //    _panel.BringIntoView();
+        //}
+        //private void FindByFirstAndLastName_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    var firstName = firstNameTextBox.Text.ToLowerInvariant();
+        //    var lastName = lastNameTextBox.Text.ToLowerInvariant();
+        //    _panel.Children.Clear();
+        //    foreach (var user in _repository.GetAll().Where(user => user.FirstName.ToLowerInvariant() == firstName && user.LastName.ToLowerInvariant() == lastName).ToList())
+        //    {
+        //        AddUserToStackPanel(user);
+        //    }
+        //    _panel.Focus();
+        //    _panel.BringIntoView();
+        //}
+        //private void FindByEmail_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    var email = emailTextBox.Text.ToLowerInvariant();
+        //    _panel.Children.Clear();
+        //    foreach (var user in _repository.GetAll().Where(user => user.Email.ToLowerInvariant() == email).ToList())
+        //    {
+        //        AddUserToStackPanel(user);
+        //    }
+        //    _panel.Focus();
+        //    _panel.BringIntoView();
+        //}
+        //private void OnLoad(object sender, RoutedEventArgs e)
+        //{
+        //}
 
-        private void AddUserToStackPanel(User user)
-        {
-            foreach (var control in AddStackPanelEntry(user!))
-            {
-                _ = _panel.Children.Add(control.Item1);
-                _ = _panel.Children.Add(control.Item2);
-            }
-        }
+        //private void AddUserToStackPanel(User user)
+        //{
+        //    foreach (var control in AddStackPanelEntry(user!))
+        //    {
+        //        _ = _panel.Children.Add(control.Item1);
+        //        _ = _panel.Children.Add(control.Item2);
+        //    }
+        //}
 
-        private static List<(Label, TextBlock)> AddStackPanelEntry(User user)
-        {
-            var clientID = CreateLableWithTextBlock("Client ID", user.ClientId.ToString());
-            var firstName = CreateLableWithTextBlock("First Name", user.FirstName);
-            var lastName = CreateLableWithTextBlock("Last Name", textBlockText: user.LastName);
-            var email = CreateLableWithTextBlock("Email", user.Email);
-            var isBusinessContact = CreateLableWithTextBlock("Is a Business Contact", textBlockText: user.IsBusinessContact.ToString());
-            var isAccountingContact = CreateLableWithTextBlock("Is a Accounting Contact", user.IsAccountingContact.ToString());
-            var isTechnicalContact = CreateLableWithTextBlock("Is a Technical Contact", user.IsTechnicalContact.ToString());
-            var hasApiAccess = CreateLableWithTextBlock("Has Api Access", textBlockText: user.HasApiAccess.ToString());
-            return new List<(Label, TextBlock)> { clientID, firstName, lastName, email, isBusinessContact, isAccountingContact, isTechnicalContact, hasApiAccess };
-        }
+        //private static List<(Label, TextBlock)> AddStackPanelEntry(User user)
+        //{
+        //    var clientID = CreateLableWithTextBlock("Client ID", user.ClientId.ToString());
+        //    var firstName = CreateLableWithTextBlock("First Name", user.FirstName);
+        //    var lastName = CreateLableWithTextBlock("Last Name", textBlockText: user.LastName);
+        //    var email = CreateLableWithTextBlock("Email", user.Email);
+        //    var isBusinessContact = CreateLableWithTextBlock("Is a Business Contact", textBlockText: user.IsBusinessContact.ToString());
+        //    var isAccountingContact = CreateLableWithTextBlock("Is a Accounting Contact", user.IsAccountingContact.ToString());
+        //    var isTechnicalContact = CreateLableWithTextBlock("Is a Technical Contact", user.IsTechnicalContact.ToString());
+        //    var hasApiAccess = CreateLableWithTextBlock("Has Api Access", textBlockText: user.HasApiAccess.ToString());
+        //    return new List<(Label, TextBlock)> { clientID, firstName, lastName, email, isBusinessContact, isAccountingContact, isTechnicalContact, hasApiAccess };
+        //}
     }
 }
---
+++ src/UserSearch/UserSearch/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,131 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+using UserSearch.Models;
+using UserSearch.Services;
+namespace UserSearch.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private readonly IRepository<User, int> _repository = new UserRepository(
+            new XmlSerializationService()
+        );
+        public ObservableCollection<User> Users { get; } = new ObservableCollection<User>();
+
+        public ICommand InitalizeUsers => new RelayCommand(() => LoadUsers(), () => true);
+        public ICommand SearchByFirstNameCommand =>
+            new RelayCommand(() => SearchUsersByFirstName(), () => true);
+        public ICommand SearchByLastNameCommand =>
+            new RelayCommand(() => SearchUsersByLastName(), () => true);
+        public ICommand SearchByFirstAndLastNameCommand =>
+            new RelayCommand(() => SearchUsersByFirstAndLastName(), () => true);
+        public ICommand SearchByEmailCommand =>
+            new RelayCommand(() => SearchUsersByEmail(), () => true);
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        private void SearchUsersByFirstName()
+        {
+            if (string.IsNullOrEmpty(FirstName))
+            {
+                MessageBox.Show(
+                    "The first name field can not be empty when searching by first name."
+                );
+                return;
+            }
+            Users.Clear();
+            var firstName = FirstName.ToLowerInvariant().Trim();
+            var users =
+                from user in _repository.GetAll()
+                where user.FirstName.ToLowerInvariant() == firstName
+                select user;
+            foreach (var user in users.ToArray())
+            {
+                Users.Add(user);
+            }
+        }
+
+        private void SearchUsersByLastName()
+        {
+            if (string.IsNullOrEmpty(LastName))
+            {
+                MessageBox.Show(
+                    "The last name field can not be empty when searching by last name."
+                );
+                return;
+            }
+            Users.Clear();
+            var lastName = LastName.ToLowerInvariant().Trim();
+            var users =
+                from user in _repository.GetAll()
+                where user.LastName.ToLowerInvariant() == lastName
+                select user;
+            foreach (var user in users.ToArray())
+            {
+                Users.Add(user);
+            }
+        }
+        private void SearchUsersByFirstAndLastName()
+        {
+            if (string.IsNullOrEmpty(FirstName))
+            {
+                MessageBox.Show(
+                    "The first name field can not be empty when searching by first and last name."
+                );
+                return;
+            }
+            if (string.IsNullOrEmpty(LastName))
+            {
+                MessageBox.Show(
+                    "The last name field can not be empty when searching by first and name."
+                );
+                return;
+            }
+            Users.Clear();
+            var firstName = FirstName.ToLowerInvariant().Trim();
+            var lastName = LastName.ToLowerInvariant().Trim();
+            var users =
+                from user in _repository.GetAll()
+                where
+                    user.FirstName.ToLowerInvariant() == firstName
+                    && user.LastName.ToLowerInvariant() == lastName
+                select user;
+            foreach (var user in users.ToArray())
+            {
+                Users.Add(user);
+            }
+        }
+        private void SearchUsersByEmail()
+        {
+            if (string.IsNullOrEmpty(Email))
+            {
+                MessageBox.Show("The email field can not be empty when searching by email.");
+                return;
+            }
+            var email = Email.ToLowerInvariant().Trim();
+            var users =
+                from user in _repository.GetAll()
+                where user.Email.ToLowerInvariant() == email
+                select user;
+            Users.Clear();
+            foreach (var user in users.ToArray())
+            {
+                Users.Add(user);
+            }
+        }
+
+        private void LoadUsers()
+        {
+            _repository.GetAll().ToList().ForEach(Users.Add);
+        }
+    }
+}
+
